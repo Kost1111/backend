8e89c098cb6a802de13a34674f6d0b66718f48ba
Konstantin <petrov_ke@resanta.ru>
2025-02-01 13:12:25 +0300

    first

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+}
+
+group = "ru.local"
+version = "0.0.1"
+
+application {
+    mainClass.set("ru.local.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.websockets)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.logback.classic)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
